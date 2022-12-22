9cf83f576e051ac93e858b77ab1ede2e384d582a
Katerina <kateclysm@gmail.com>
2022-12-22 14:13:00 -0300

    Separación del Front y Back.

--- FRONTEND/src/app/model/skill.ts
+++
@@ -1,8 +0,0 @@
-export class Skill {
-    id: number;
-    nombre: string;
-
-    constructor(nombre:string){
-        this.nombre = nombre;
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio
-Repositorio dedicado al Frontend y Backend de mi Portfolio Web desarrollado durante el Plan Programar Argentina.
